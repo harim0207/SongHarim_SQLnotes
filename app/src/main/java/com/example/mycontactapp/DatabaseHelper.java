d29ca52aaa7ef0f614cf4459e15d7f42ebc60940
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-05-23 10:14:20 -0700

    android studio in progress

---
+++ app/src/main/java/com/example/mycontactapp/DatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.mycontactapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Contact2019.db";
+    public static final String TABLE_NAME = "Contact2019_table";
+    public static final String ID = "ID";
+    public static final String COLUMN_NAME_CONTACT = "contact";
+
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_NAME_CONTACT + " TEXT)";
+
+    public static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null,  1);
+        SQLiteDatabase db = this.getWritableDatabase(); //FOR TEST ONLY - REMOVE LATER
+        Log.d("MyContactApp", "DatabaseHelper: constructed the DatabaseHelper");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
